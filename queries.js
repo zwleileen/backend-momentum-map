9f072a6c6a2e5c9790eef4c2a8c7d7d658233123
Amos Yeo <amosyeo.sh@gmail.com>
2025-02-14 21:14:18 +0800

    commit first. forgot to use branch. psps.

---
+++ queries.js
@@ -0,0 +1,31 @@
+/*------------------------------- Starter Code -------------------------------*/
+
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+const User = require("./models/user");
+const Hoot = require("./models/hoot");
+const bcrypt = require("bcrypt");
+
+const connect = async () => {
+  // Connect to MongoDB using the MONGODB_URI specified in our .env file.
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log("Connected to MongoDB");
+
+  // Call the runQueries function, which will eventually hold functions to work
+  // with data in our db.
+  await runQueries();
+
+  // Disconnect our app from MongoDB after our queries run.
+  await mongoose.disconnect();
+  console.log("Disconnected from MongoDB");
+
+  // Close our app, bringing us back to the command line.
+  process.exit();
+};
+
+const runQueries = async () => {
+  console.log(`runQueris is running.`);
+
+  connect();
+};
