74195cc5a5580fdd3772889b74baa9d001ef8512
eileen <zwleileen@gmail.com>
2025-02-17 20:52:03 +0800

    managed to get values

--- controllers/values.js
+++ controllers/values.js
@@ -14,13 +14,24 @@ router.post("/new", verifyToken, async (req, res) => {
   }
 });
 
-router.get("/", verifyToken, async (req, res) => {
+router.get("/:valuesId", verifyToken, async (req, res) => {
   try {
-    const values = await Value.find({})
-      .populate("name")
-      .sort({ createdAt: "desc" });
+    const values = await Value.findById(req.params.valuesId).populate("name");
+
+    if (!values) {
+      return res.status(404).json({ error: "Values not found for this user" });
+    }
+
+    console.log("Found values:", values);
+
+    // Check permissions:
+    if (values.name._id.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ error: "Unauthorized access!" });
+    }
+
     res.status(200).json(values);
   } catch (error) {
+    console.error("Error fetching values:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
