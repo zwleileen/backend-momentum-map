10fdc8c99ce789cb1fe097818fe42e6c6ee41bef
eileen <zwleileen@gmail.com>
2025-02-24 13:16:48 +0800

    managed to delete message

--- controllers/messages.js
+++ controllers/messages.js
@@ -38,4 +38,22 @@ router.get("/:userId", verifyToken, async (req, res) => {
   }
 });
 
+router.delete("/:messageId", verifyToken, async (req, res) => {
+  try {
+    const deletedMessage = await Message.findByIdAndDelete(
+      req.params.messageId
+    );
+
+    if (!deletedMessage) {
+      return res.status(404).json({ error: "Message not found." });
+    }
+
+    console.log("Deleted message:", deletedMessage);
+    res.status(200).json({ message: "Message deleted successfully." });
+  } catch (error) {
+    console.error("Error deleting message:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 module.exports = router;
